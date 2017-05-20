3cce2614cff960c339cd380686bebcc5a672cd69
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-05-19 22:29:34 -0400

    Initial Commit

---
+++ Intro/1-Add/Add/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Add(5, 8);
+            Console.WriteLine(result);
+        }
+        public static int Add(int param1, int param2)
+        {
+            return param1 + param2;
+        }
+    }
+}
