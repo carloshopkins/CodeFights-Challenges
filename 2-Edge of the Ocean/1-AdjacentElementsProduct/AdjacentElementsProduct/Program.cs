1237da1d6e75ba1adc56efba55a310868cb7ac34
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-05-20 13:04:10 -0400

    Added AdjacentElementsProduct

---
+++ 2-Edge of the Ocean/1-AdjacentElementsProduct/AdjacentElementsProduct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdjacentElementsProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray =  { 3, 6, -2, -5, 7, 3 };
+            Console.WriteLine(AdjacentProduct(inputArray));
+        }
+        public static int AdjacentProduct(int[] inputArr)
+        {
+            int result = inputArr[0] * inputArr[1];
+            for (int i = 0; i < inputArr.Length - 1; i++)
+            {
+                if (inputArr[i] * inputArr[i + 1] > result)
+                    result = inputArr[i] * inputArr[i + 1];
+            }
+            return result;
+        }
+    }
+}
