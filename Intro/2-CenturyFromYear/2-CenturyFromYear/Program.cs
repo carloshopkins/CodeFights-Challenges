3edc8ad324a87777a1d57af77da43fee54ba98b0
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-05-19 23:51:23 -0400

    Initial Commit

---
+++ Intro/2-CenturyFromYear/2-CenturyFromYear/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _2_CenturyFromYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindCentury(1710));
+            Console.WriteLine(FindCentury(1700));
+        }
+        public static int FindCentury(int year)
+        {
+            var century = year / 100;
+
+            if ((year % 100) == 0)
+                return century;
+            else
+                return century + 1;
+        }
+    }
+}
---
+++ Intro/3-CheckPalinprome/3-CheckPalinprome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CheckPalinprome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckForPalindrome("apple"));
+            Console.WriteLine(CheckForPalindrome("aabAa"));
+            Console.WriteLine(CheckForPalindrome("racecar"));
+            Console.WriteLine(CheckForPalindrome("nope"));
+            Console.WriteLine(CheckForPalindrome("ama"));
+        }
+        public static bool CheckForPalindrome(string input)
+        {
+            var inputArr = input.ToLower().ToCharArray();
+            
+            return inputArr.SequenceEqual(inputArr.Reverse());
+        }
+    }
+}
