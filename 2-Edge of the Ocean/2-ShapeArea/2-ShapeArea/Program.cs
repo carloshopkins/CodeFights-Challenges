41647adaa073e371e7a5b571151b67da4a7993d9
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-05-20 13:52:53 -0400

    Added ShapeArea

---
+++ 2-Edge of the Ocean/2-ShapeArea/2-ShapeArea/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_ShapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ShapeArea(1));
+            Console.WriteLine(ShapeArea(2));
+            Console.WriteLine(ShapeArea(3));
+            Console.WriteLine(ShapeArea(4));
+            Console.WriteLine(ShapeArea(5));
+        }
+        public static int ShapeArea(int i)
+        {
+            return 2 * i * (i - 1) + 1;
+        }
+    }
+}
