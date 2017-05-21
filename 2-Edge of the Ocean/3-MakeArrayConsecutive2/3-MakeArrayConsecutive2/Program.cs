093c228c0ec8a5d06c7c89c262a99867b6badad3
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-05-20 22:00:54 -0400

    Added MakeArrayConsecutive2

---
+++ 2-Edge of the Ocean/3-MakeArrayConsecutive2/3-MakeArrayConsecutive2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _3_MakeArrayConsecutive2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] statues = {6, 2, 3, 8};
+            Console.WriteLine(FindStatues(statues)); 
+        }
+        public static int FindStatues(int[] statues)
+        {
+            Array.Sort(statues);
+            //min and max variables were not included in the verision submitted on 
+            //Codefighters, but I included them here to help improve readability.
+            var min = statues[0];
+            var max = statues[statues.Length - 1];
+            var compareTo = new List<int>();
+            
+            for (int i = min; i <= max; i++)
+            {
+                compareTo.Add(i);
+            }
+
+            return compareTo.Count - statues.Length;
+        }
+    }
+}
