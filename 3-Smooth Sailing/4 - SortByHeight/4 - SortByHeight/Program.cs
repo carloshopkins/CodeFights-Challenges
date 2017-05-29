07f62d7cd054064707eff62a65f66552c93f3004
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-05-29 13:29:36 -0400

    Added SortByHeight

---
+++ 3-Smooth Sailing/4 - SortByHeight/4 - SortByHeight/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4___SortByHeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArr = { 1, 4, -1, 9, -1 , 100, -1, 8, 11, 19, 3 };
+
+            var resultArr = SortByHeight(testArr);
+
+            foreach (var i in resultArr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        public static int[] SortByHeight(int[] a)
+        {
+            var arr2 = a.Where(i => i != -1).ToList();
+            arr2.Sort();
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (a[i] == -1)
+                {
+                    arr2.Insert(i, -1);
+                }
+            }
+            return arr2.ToArray();
+        }
+    }
+}
