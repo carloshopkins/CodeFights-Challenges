5a6c5fa46b29c79d759e13a4675cfbcc92755c51
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-05-30 11:23:46 -0400

    Added Reverse Parentheses

---
+++ 3-Smooth Sailing/5 - ReverseParentheses/ReverseParenthese.js
@@ -0,0 +1,12 @@
+string reverseParentheses(string s) {
+        var reverse = (str) => str.split('').reverse().join('');
+
+    var reverseParentheses = (s) => {
+        while (s.includes('(')) {
+            var l = s.lastIndexOf('(');
+            var r = s.indexOf(')', s.lastIndexOf('('));
+            s = s.slice(0, l) + reverse(s.slice(l + 1, r)) + (r + 1 === s.length ? s.slice(r, -1) : s.slice(r + 1));
+        }
+        return s;
+    };
+};
