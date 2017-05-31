09e6fe36a7fba1c30bf9e2f9737ba72d08b61840
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-05-31 08:02:35 -0400

    Added Alternating Sums and Add Border

---
+++ 4-Exploring the Waters/1 - AlternatingSums/1 - AlternatingSums/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1___AlternatingSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArr = { 40, 50, 10, 100, 150 };
+            int[] testArr2 = { 75, 80, 50, 90, 200, 110 };
+
+            foreach (var i in AlternatingSums(testArr)) 
+                Console.Write(i + " ");
+            Console.WriteLine();
+            foreach (var i in AlternatingSums(testArr2))
+                Console.Write(i + " ");
+        }
+        public static int[] AlternatingSums(int[] a)
+        {
+            var result = new int[2];
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (i % 2 == 0 || i == 0)
+                    result[0] += a[i];
+                else
+                    result[1] += a[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ 4-Exploring the Waters/2 - Add Border/2 - Add Border/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___Add_Border
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] testArr = {"abc", "ded"};
+            string[] testArr2 = { "a" };
+
+            foreach (var i in addBorder(testArr))
+                Console.WriteLine(i);
+            foreach (var i in addBorder(testArr2))
+                Console.WriteLine(i); 
+        }
+        public static string[] addBorder(string[] picture)
+        {
+            var result = picture.ToList();
+            var borderString = "";
+            for (var i = 0; i < result.Count; i++)
+            {
+                result[i] = "*" + result[i] + "*";
+            }
+            for (var i = 0; i < result[0].Length; i++)
+            {
+                borderString += "*";
+            }
+            result.Insert(0, borderString);
+            result.Insert(result.Count, borderString);
+            return result.ToArray();
+        }
+    }
+}
