82f7810848761a294368346138f634f5ef54571e
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-06-03 10:28:25 -0400

    Added ArrayChange

---
+++ 4-Exploring the Waters/4 - ArrayChange/4 - ArrayChange/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4___ArrayChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArr = { -1000, 0, -2, 0 };
+            int[] testArr2 = { 1, 1, 1 };
+            int[] testArr3 = { 2, 3, 3, 5, 5, 5, 4, 12, 12, 10, 15 };
+
+            Console.WriteLine("Expected: 5 | Result: " + arrayChange(testArr));
+            Console.WriteLine("Expected: 3 | Result: " + arrayChange(testArr2));
+            Console.WriteLine("Expected: 13 | Result: " + arrayChange(testArr3));
+        }
+        public static int arrayChange(int[] inputArray)
+        {
+            var count = 0;
+            for (var i = 1; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] <= inputArray[i - 1])
+                {
+                    count += inputArray[i - 1] + 1 - inputArray[i];
+                    inputArray[i] = inputArray[i - 1] + 1;
+                }
+            }
+            return count;
+        }
+    }
+}
