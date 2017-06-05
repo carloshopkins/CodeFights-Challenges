09659d92384368aa0fdfdd6c0a360a06f7bb628b
Carlos Alexander Hopkins <Chopkins95@live.com>
2017-06-05 08:49:21 -0400

    Added AreEqualyStrong

---
+++ 5 - Island of Knowledge/1 - AreEquallyStrong/1 - AreEquallyStrong/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1___AreEquallyStrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Expected: True | Result: " + areEquallyStrong(10, 15, 15, 10));
+            Console.WriteLine("Expected: True | Result: " + areEquallyStrong(15, 10, 15, 10));
+            Console.WriteLine("Expected: False | Result: " + areEquallyStrong(15, 10, 15, 9));
+        }
+        public static bool areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight)
+        {
+            return (yourLeft == friendsLeft && yourRight == friendsRight || yourLeft == friendsRight && yourRight == friendsLeft);
+        }
+    }
+}
